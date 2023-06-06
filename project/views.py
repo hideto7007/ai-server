46c6c6acfc56cf4235bab1cb2a9364f4b259f76b
hideto tsuzuki <ma_kux@icloud.com>
2023-06-06 21:16:15 +0900

    プロジェクト削除API実装

--- project/views.py
+++ project/views.py
@@ -9,7 +9,7 @@ from project.common import (
     get_project_model_list,
     update_project_request
 )
-from const.const import ApiResultKind, ObjectDetectionModelColumn
+from const.const import ApiResultKind, ProjectColumn
 from project.models import Project
 
 
@@ -40,35 +40,37 @@ class ProjectlListAPIView(views.APIView):
 
 
 
-# class ObjectDetectionModelDeleteAPIView(views.APIView):
-#     """物体検知モデルデータ削除APIクラス"""
-#
-#     def post(self, request, *args, **kwargs):
-#         """物体検知モデルデータ削除"""
-#
-#         if check_login(request.GET.get("username"), request.GET.get("token"), request.GET.get("user_id")):
-#             result_code = ApiResultKind.RESULT_SUCCESS
-#             result_array = delete_request(ObjectDetectionModel,
-#                                           ObjectDetectionModelColumn.ID.value,
-#                                           request.data[ObjectDetectionModelColumn.ID.value])
-#
-#             if len(result_array) == 0:
-#                 result_code = result_code
-#                 message = "Success"
-#             else:
-#                 result_code = ApiResultKind.RESULT_ERROR
-#                 message = "DB登録データ削除エラー"
-#
-#             return JsonResponse(
-#                 {"result_code": result_code,
-#                  "message": message}, safe=False
-#             )
-#         else:
-#             request = {"result_code": 1, "message": "セッションの有効期限が切れています。"}
-#
-#             return JsonResponse(request, safe=False)
-#
-#
+class ProjectDeleteAPIView(views.APIView):
+    """プロジェクトデータ削除APIクラス"""
+
+    def post(self, request, *args, **kwargs):
+        """物体検知モデルデータ削除"""
+
+        if check_login(request.data["params"][0]["username"],
+                       request.data["params"][0]["token"],
+                       request.data["params"][0]["user_id"]):
+            result_code = ApiResultKind.RESULT_SUCCESS
+            result_array = delete_request(Project,
+                                          ProjectColumn.ID.value,
+                                          request.data[ProjectColumn.ID.value])
+
+            if len(result_array) == 0:
+                result_code = result_code
+                message = "Success"
+            else:
+                result_code = ApiResultKind.RESULT_ERROR
+                message = "DB登録データ削除エラー"
+
+            return JsonResponse(
+                {"result_code": result_code,
+                 "message": message}, safe=False
+            )
+        else:
+            request = {"result_code": 1, "message": "セッションの有効期限が切れています。"}
+
+            return JsonResponse(request, safe=False)
+
+
 class ProjectPostListAPIView(views.APIView):
     """プロジェクトデータ登録・更新APIクラス"""
 
